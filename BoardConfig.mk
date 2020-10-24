dc55e3eae456f5d7ca91ed18cf94250c17c0a453
SparXFusion <s2234nadar@gmail.com>
2020-10-25 03:31:13 +0530

    RMX1831: address spm firmware denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,5 +149,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 -include vendor/realme/RMX1831/BoardConfigVendor.mk
