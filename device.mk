1d72bb5e41f0db6b8556b94e5306b94885eeba8d
Kshitij Gupta <kshitijgm@gmail.com>
2020-11-01 20:33:25 +0100

    RMX1831: Use POSP apns config
    - LineageOS one lacks of IMS support for a lot of carriers

--- device.mk
+++ device.mk
@@ -43,6 +43,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/misc/factory.ini:$(TARGET_COPY_OUT_SYSTEM)/etc/factory.ini \
     $(LOCAL_PATH)/misc/custom.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/custom.conf
 
+# APNs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.mt6771.rc \
