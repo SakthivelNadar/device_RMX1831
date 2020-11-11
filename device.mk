a385db891f356eebe46f37bab992e017556c9270
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2020-11-11 17:51:05 +0530

    RMX1831: remove all conflicting ims/telephony packages

--- device.mk
+++ device.mk
@@ -156,16 +156,7 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.RMX1831
 
 # IMS
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml
-
-# Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext
-
 PRODUCT_BOOT_JARS += \
-    telephony-ext \
     mediatek-common \
     mediatek-framework \
     mediatek-ims-base \
